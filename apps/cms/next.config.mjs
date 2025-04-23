34163d0bfba367417f2f4fb34a38871c60458e44
Dean <cheoljin.an@lotte.net>
2025-04-23 18:23:04 +0900

    fix: some...

--- apps/cms/next.config.mjs
+++ apps/cms/next.config.mjs
@@ -3,6 +3,17 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
+  webpack: (config, { isServer }) => {
+    // Postgres 관련 cloudflare:sockets 문제 해결
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        'pg-cloudflare': false,
+        'cloudflare:sockets': false,
+      }
+    }
+    return config
+  },
 }
 
 export default withPayload(nextConfig, { devBundleServerPackages: false })
